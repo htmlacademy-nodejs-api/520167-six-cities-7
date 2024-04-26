792c4411bf4d92919b3bf389892d12163b34e7b8
Golovin Yurii <golovin@reksoft.ru>
2024-04-26 19:36:02 +0300

    Добавлено приложение для терминала

---
+++ src/shared/types/index.ts
@@ -0,0 +1,7 @@
+export { Comment } from './comment.type.js';
+export { CityType } from './city-type.enum.js';
+export { FacilityType } from './facility-type.enum.js';
+export { HousingType } from './housing-type.enum.js';
+export { Offer } from './offer.type.js';
+export { User } from './user.type.js';
+export { UserType } from './user-type.enum.js';
